2712a9c026c3ccd6bf178c7745bc7650c7c15b3c
nirmalsingh7950 <nirmalsingh7950@gmail.com>
2020-03-10 02:46:09 +0530

    Version 0.3 added

--- Script_Version_0.3.sh
+++ Script_Version_0.3.sh
@@ -2,32 +2,28 @@
 
 if [ $# -gt 0 ] ; then
 #copy first common lines upto colors and write it in merge file
-sed -n '/apiVers/,/  colors:/p' $1 > merge
+sed -n '/apiVers/,/  colors:/p' $2 > $1
 
-mergeFile=$1
 
-touch $mergeFile
-
-touch temp2
 #CLUSTER BLOCK
-echo "clusters:" >> temp2
+echo "clusters:" >> $1
 for arg
 do 
-	if [ $1 ] ; then
-	     break
+	if [ "$arg" =  "$1" ] ; then
+		continue
 	else
-		sed -nr '/- name: clus/,/contexts:/p' $arg | sed '$d' >> temp2
+		sed -nr '/- name: clus/,/contexts:/p' $arg | sed '$d' >> $1
 	fi
 done
 ##################################################
 #CONTEXTS BLOCK
-echo "contexts:" >> temp2
+echo "contexts:" >> $1
 for arg
 do
-	 if [ $1 ] ; then
-             break
-     else
-        sed -nr '/context:/,/current-context:/p' $arg | sed '$d' >> temp2
+	if [ "$arg" =  "$1" ] ; then
+                continue
+        else
+       		sed -nr '/context:/,/current-context:/p' $arg | sed '$d' >> $1
 	fi
 done
 ###################################################################
@@ -35,28 +31,22 @@ done
 touch temp
 for arg
 do 
-	 if [ $1 ] ; then
-             break
-	else
 	sed -nr '/current-context:/p' $arg >> temp
-fi
 done
 # randomly picking the current context from temp file and append it to the end of merge file
-shuf -n 1 temp >> temp2
+shuf -n 1 temp >> $1
 rm temp
 ####################################################################################
 #USERS BLOCK
-echo "users:" >> temp2
+echo "users:" >> $1
 for arg
 do
-	 if [ $1 ] ; then
-             break
-     else
-	 sed -n '/users:/,$ p' $arg | sed '1d' >> temp2
- fi
+	if [ "$arg" =  "$1" ] ; then
+                continue
+        else
+ 		sed -n '/users:/,$ p' $arg | sed '1d' >> $1
+	fi
 done
-cp temp2 $mergeFile
-rm temp2
 
 
 else
